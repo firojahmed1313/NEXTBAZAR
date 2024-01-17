2da3204ba88225af6304acdccc7b758a0adfea45
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-01-17 06:04:03 +0000

    audit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     },
     env:{
         DB_URL:"mongodb+srv://newgitpods:VL6eb0rHhdZJ1gv0@mdfirojahmed.chpjhjr.mongodb.net/NEXTBAZAR?retryWrites=true&w=majority",
-        API_URL:"https://3000-mdfirojahmed-nextebazar-3rent4g91jw.ws-us107.gitpod.io"
+        API_URL:"https://3000-firojahmed131-nextbazar-dkc8p4kpghc.ws-us107.gitpod.io",
     },
     images: {
         domains: ["res.cloudinary.com"],
